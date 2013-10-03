b1b699cb8e22b98d7ecf488013adeb7ce64942cb
liyonghelpme <liyonghelpme@gmail.com>
2013-10-03 16:13:33 +0800

    is dir check first

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ def transFile(cur):
     mvFiles = []
     for i in allF:
         fn = os.path.join(cur, i)
-        if i.find('.png') != -1 or i.find('.jpg') != -1 or i.find('.mp3') != -1 or i.find('.fnt') != -1 or i.find(".plist") != -1:
+        if os.path.isdir(fn):
+            transFile(fn)
+        elif i.find('.png') != -1 or i.find('.jpg') != -1 or i.find('.mp3') != -1 or i.find('.fnt') != -1 or i.find(".plist") != -1:
             os.system('python genMd5.py %s; mv %s ../images' % (fn, fn))
             mvFiles.append(i)
-        elif os.path.isdir(fn):
-            transFile(fn)
     print "mvFiles", mvFiles
         
 @app.route('/', methods=['GET', 'POST'])
